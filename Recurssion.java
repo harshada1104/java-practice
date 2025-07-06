cd45b8e78f4567f04ce978b389262a2fe988d934
Harshada Bhandare <nehabhandare2278@gmail.com>
2025-07-06 23:10:16 +0530

    recurssion

--- Recurssion.java
+++ Recurssion.java
@@ -1,101 +1,103 @@
 public class Recurssion {
     
-    public static void main(String[] args) {
-       RecurssionName(0); 
-       linear(3);
+    static int cnt = 0;
+
+    //printing something for desire times
+    static void print(){
+
+        //base condition
+        if(cnt == 3)return;
+        System.out.println(cnt);
+
+        //count increment
+        cnt = cnt + 1;
+        print();
+
     }
-//printing name 5times
-    static void RecurssionName(int count){
-   
-      if(count >= 5){
-        return;
-      }else{
+
+    //print name n times
+    static void printname( int i , int n){
+
+        //base condition
+        if (i>=n)return;
         System.out.println("harshada");
+
+
+        //incremment
         
-        RecurssionName(count+1);
-      }
+        printname(i+1, n);
     }
-//print linearly from 1 to n
-static void linear(int n){
-    int deductor = 0;
-   if (deductor==5){
-   return;
-   }else{
-    System.out.println(deductor);
-  
-     linear( deductor++);
-   }
+
+       //print name 1-n 
+    static void print1( int i , int n){
+
+        //base condition
+        if (i>=n)return;
+        System.out.println(i);
+
+
+        //incremment
+        
+        print1(i+1, n);
+    }
+
+    //print no from n-1
+ static void printN1( int i , int n){
+
+        //base condition
+        if (i<1)return;
+        System.out.println(i);
+
+
+        printN1(i-1, n);
+ }
+ //print sum of first n numbers
+  static void printSum( int i , int n, int sum){
+
+        //base condition
+        if (i>=n)return;
+         sum =  sum + i;
+         System.out.println(sum);
+
+        printSum(i+1, n,sum );
+        
+ }
+
+ //print the reverse array
+  //function to print an array
+  static void printArray(int ans[],int n){
+    System.out.println("reversed array is ");
+    for(int i = 0;i<n;i++){
+        System.out.println(ans[i]+" ");
+    }
+  }
+
+  static void reverseArray(int arr[],int start,int end){
+ if(start<end){
+    int tmp = arr[start];
+    arr[start]=arr[end];
+    arr[end]=tmp;
+    reverseArray(arr, start+1, end-1);
+ }
+  }
+
+
+    
+    
+    public static void main(String[] args) {
+    //   print();
+    //   printname(0, 4);
+    //   print1(0, 4);
+    //   int n = 4;
+    //   printN1(n, n);
+    //   printSum(0, 4, 0);
+      int n = 5;
+      int arr[]={5,6,78,2,8};
+      reverseArray(arr, 0, n-1);
+      printArray(arr, n);
+    }
+
+
+    
 }
 
-static void linear1(int n){
-    int deductor = 0;
-   if (deductor==5){
-   return;
-   }else{
-    System.out.println(deductor);
-  
-     linear( deductor++);
-   }
-}
-static void linear2(int n){
-    int deductor = 0;
-   if (deductor==5){
-   return;
-   }else{
-    System.out.println(deductor);
-  
-     linear( deductor++);
-   }
-}
-static void linear4(int n){
-    int deductor = 0;
-   if (deductor==5){
-   return;
-   }else{
-    System.out.println(deductor);
-  
-     linear( deductor++);
-   }
-}
-static void linear5(int n){
-    int deductor = 0;
-   if (deductor==5){
-   return;
-   }else{
-    System.out.println(deductor);
-  
-     linear( deductor++);
-   }
-}
-static void linear6(int n){
-    int deductor = 0;
-   if (deductor==5){
-   return;
-   }else{
-    System.out.println(deductor);
-  
-     linear( deductor++);
-   }
-}
-static void linear7(int n){
-    int deductor = 0;
-   if (deductor==5){
-   return;
-   }else{
-    System.out.println(deductor);
-  
-     linear( deductor++);
-   }
-}
-
-static void linear8(int n){
-    int deductor = 0;
-   if (deductor==5){
-   return;
-   }else{
-    System.out.println(deductor);
-  
-     linear( deductor++);
-   }
-}
-}
