953e6500bc5e2ccc925f8f3cf561e331d46a5c4d
Harshada Bhandare <nehabhandare2278@gmail.com>
2025-07-09 22:04:58 +0530

    recurssion

--- Recurssion.java
+++ Recurssion.java
@@ -99,6 +99,15 @@ public class Recurssion {
  }
   }
 
+    static void reverseArray3(int arr[],int start,int end){
+ if(start<end){
+    int tmp = arr[start];
+    arr[start]=arr[end];
+    arr[end]=tmp;
+    reverseArray(arr, start+1, end-1);
+ }
+  }
+
     
     
     public static void main(String[] args) {
