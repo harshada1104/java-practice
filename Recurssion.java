51285ad20c56e28a16c724c1cebd2c9c81426620
Harshada Bhandare <nehabhandare2278@gmail.com>
2025-07-19 23:31:41 +0530

    added linear regression in frontend , solved the youtube data error

--- Recurssion.java
+++ Recurssion.java
@@ -165,6 +165,14 @@ public class Recurssion {
  }
 }
 
+ static void reverseArray61(int arr[],int start,int end){
+ if(start<end){
+    int tmp = arr[start];
+    arr[start]=arr[end];
+    arr[end]=tmp;
+    reverseArray(arr, start+1, end-1);
+ }
+}
     
     public static void main(String[] args) {
     //   print();
