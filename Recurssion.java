03a838cba2da1d2dcde79d23715e16263097071d
Harshada Bhandare <nehabhandare2278@gmail.com>
2025-08-06 21:38:48 +0530

    new models includede

--- Recurssion.java
+++ Recurssion.java
@@ -100,6 +100,12 @@ public class Recurssion {
         System.out.println(ans[i]+" ");
     }
   }
+      static void printArray20(int ans[],int n){
+    System.out.println("reversed array is ");
+    for(int i = 0;i<n;i++){
+        System.out.println(ans[i]+" ");
+    }
+  }
 
    //print the reverse array
   static void reverseArray(int arr[],int start,int end){
