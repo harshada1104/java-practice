91d6cf0337be2145d4437648cec49a2d1939b681
Harshada Bhandare <nehabhandare2278@gmail.com>
2025-08-13 22:08:11 +0530

    new models includede

---
+++ Hashmap.java
@@ -0,0 +1,10 @@
+public class Hashmap {
+   public static void main(String[] args) {
+    
+System.out.println("Hello, World! This is a Hashmap example Hashmap example = new Hashmap();  example.runExample();  ");
+
+
+   } 
+
+
+}
