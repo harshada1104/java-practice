a51a41734871d155f0acc084379b7875563ad179
Harshada Bhandare <nehabhandare2278@gmail.com>
2025-09-02 11:12:29 +0530

    Your meaningful commit message

---
+++ Strings1/Ransome.java
@@ -0,0 +1,20 @@
+package Strings1;
+
+public class Ransome {
+    public static void main(String[] args) {
+      String ransomNote = "abcc";
+      String magazine = "bccc"  ;
+      ransome(ransomNote, magazine);
+    }
+
+    public static void ransome(String s,String m){
+        for(int i =0;i<m.length();i++){
+for(int j=0;j<s.length();j++){
+    if(m.charAt(i)==s.charAt(j)){
+        System.out.println(s.charAt(j));
+    }
+}
+        }
+    }
+
+}
