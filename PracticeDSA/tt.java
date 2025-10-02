d3084ae98561da2af6e67fce60a50d51f9f9963d
Harshada Bhandare <nehabhandare2278@gmail.com>
2025-10-02 23:58:21 +0530

    done with 4 problems

---
+++ PracticeDSA/tt.java
@@ -0,0 +1,39 @@
+package PracticeDSA;
+
+public class tt {
+    public static int search(int arr[],int n,int x){
+        int low=0,high=n-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(arr[mid]==x){
+                return mid;
+            }
+             if (arr[low] == arr[mid] && arr[mid] == arr[high]) {
+                low = low + 1;
+                high = high - 1;
+                continue;
+            }
+            if(arr[low]<=arr[mid]){
+                if(arr[low]<=x && x<=arr[mid]){
+                    high=mid-1;
+                }else{
+                    low=mid+1;
+                }
+            }else{
+                if(arr[mid]<=x && x<=arr[high]){
+                    low=mid+1;
+                }else{
+                    high=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int arr[] ={3,8,9,3,3,3,3};
+        int n = arr.length;
+        int x = 8;
+       int result = search(arr, n, x);  
+        System.out.println(result);
+    }
+}
